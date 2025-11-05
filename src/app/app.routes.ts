96de805a87ffa69cafb3b517226148defbbabac4
Joao <joaope.turina@gmail.com>
2025-11-04 21:06:36 -0300

    adcionando parte do login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { Login } from './feature/users/components/login/login/login';
+import { Login } from './features/users/components/login/login/login';
 
 export const routes: Routes = [
   {
